779d4b419944d1d19f2aab471900919f519b5fd2
Em01 <emma_williams1@hotmail.co.uk>
2014-10-21 15:10:26 +0100

    winning possibilities

--- tic.rb
+++ tic.rb
@@ -9,3 +9,17 @@
   "b1"=>" ","b2"=>" ","b3"=>" ",
   "c1"=>" ","c2"=>" ","c3"=>" "
 }
+
+#now we need to define the 8 winning possibilities
+#these can be stored wihtin an array which is called @columns where each index is a nested array and defines the positions needed to win.
+
+@columns = [
+  ['a1','a2','a3'],
+  ['b1','b2','b3'],
+  ['c1','c2','c3'],
+  ['a1','b1','c1'],
+  ['a2','b2','c2'],
+  ['a3','b3','c3'],
+  ['a1','b2','c3'],
+  ['c1','b2','a3']
+]
