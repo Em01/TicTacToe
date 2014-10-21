3edb1c692768297a9a0b463e13067a5580541f58
Em01 <emma_williams1@hotmail.co.uk>
2014-10-21 15:08:10 +0100

    initial commit

---
+++ tic.rb
@@ -0,0 +1,11 @@
+
+#start by defining our initalize method
+#first want to create a container to hold the 9 places on the board
+#3 by 3 grif so we will name them A,B,C accross and 1,2,3 down
+#these are then stored in a has object and their default values are set to spaces indicating that they are empty
+#the 9 slots are then defined below
+@places = {
+  "a1"=>" ","a2"=>" ","a3"=>" ",
+  "b1"=>" ","b2"=>" ","b3"=>" ",
+  "c1"=>" ","c2"=>" ","c3"=>" "
+}
